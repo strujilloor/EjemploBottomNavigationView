f9124504f8a5a9da21677250996bfc0cd516c8ce
Stiven Trujillo <strujillo1293@gmail.com>
2018-05-25 08:39:43 -0500

    Fragments implementados

--- app/src/main/java/com/stiven/ejemplobottomnavigationview/MainActivity.java
+++ app/src/main/java/com/stiven/ejemplobottomnavigationview/MainActivity.java
@@ -1,11 +1,17 @@
 package com.stiven.ejemplobottomnavigationview;
 
+import android.app.FragmentTransaction;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toast;
+
+import android.support.v4.app.Fragment;
+
+import com.stiven.ejemplobottomnavigationview.view.fragment.HomeFragment;
+import com.stiven.ejemplobottomnavigationview.view.fragment.Tab2Fragment;
+import com.stiven.ejemplobottomnavigationview.view.fragment.Tab3Fragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,22 +26,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        replaceFragment(new HomeFragment());
+
         navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
                 switch (item.getItemId()){
                     case R.id.action_home:
-                        Toast.makeText(MainActivity.this, "Home Touched",Toast.LENGTH_SHORT).show();
+                        fragment = new HomeFragment();
                         break;
                     case R.id.action_tab2:
-                        Toast.makeText(MainActivity.this, "Tab2 Touched",Toast.LENGTH_SHORT).show();
+                        fragment = new Tab2Fragment();
                         break;
                     case R.id.action_tab3:
-                        Toast.makeText(MainActivity.this, "Tab3 Touched",Toast.LENGTH_SHORT).show();
+                        fragment = new Tab3Fragment();
                         break;
                 }
+                replaceFragment(fragment);
                 return true;
             }
         });
+    }
+
+    public void replaceFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .addToBackStack(null).commit();
     }
 }
